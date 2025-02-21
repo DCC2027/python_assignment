81ce9e8f7476d5ce88ca4c72f182be04a3609451
DCC2027 <conglin@hotmail.de>
2025-02-22 00:13:16 +0100

    bugfix + docstring

--- dump_dataset.py
+++ dump_dataset.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import sqlite3
 
-# 读取 CSV 文件
+# read csv as dataframe
 train_df = pd.read_csv("/Users/lincong/Desktop/python_course/assignment/Dataset/train.csv")
 test_df = pd.read_csv("/Users/lincong/Desktop/python_course/assignment/Dataset/test.csv")
 ideal_df = pd.read_csv("/Users/lincong/Desktop/python_course/assignment/Dataset/ideal.csv")
 
-# 连接 SQLite 数据库
+# connect to SQLite database
 conn = sqlite3.connect("/Users/lincong/Desktop/python_course/assignment/Dataset/functions.db")
 cursor = conn.cursor()
 
-# 创建表
+# create table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS train_data (
     X REAL PRIMARY KEY,
@@ -35,22 +35,22 @@ CREATE TABLE IF NOT EXISTS ideal_functions (
 );
 """)
 
-# 存入数据库
+# save dataframes into dataset
 train_df.to_sql("train_data", conn, if_exists="replace", index=False)
 test_df.to_sql("test_data", conn, if_exists="replace", index=False)
 ideal_df.to_sql("ideal_functions", conn, if_exists="replace", index=False)
 
+print("Data loading completed!")
 
-
-
-print("数据加载完成！")
 
 def dump_unit_test():
+    """ unit test: sanity check, whether data dumped into database and consistent with original csv file
+    """
     print("===============performing unit test================")
     train_df = pd.read_csv("/Users/lincong/Desktop/python_course/assignment/Dataset/train.csv")
     cursor.execute("SELECT * FROM train_data LIMIT 1;")
     row = cursor.fetchone()
-    expected_row = train_df.iloc[0]  # 获取 CSV 第一行数据并转换成元组
+    expected_row = train_df.iloc[0]  
     sanity_check = True
     for i in range(len(row)):
         if row[i] != expected_row.iloc[i]:
@@ -61,10 +61,10 @@ def dump_unit_test():
         print("train_df unit test failed. Program exit.")
         exit()
     
-    train_df = pd.read_csv("/Users/lincong/Desktop/python_course/assignment/Dataset/test.csv")
+    test_df = pd.read_csv("/Users/lincong/Desktop/python_course/assignment/Dataset/test.csv")
     cursor.execute("SELECT * FROM test_data LIMIT 1;")
     row = cursor.fetchone()
-    expected_row = test_df.iloc[0]  # 获取 CSV 第一行数据并转换成元组
+    expected_row = test_df.iloc[0]  
     sanity_check = True
     for i in range(len(row)):
         if row[i] != expected_row.iloc[i]:
@@ -75,10 +75,10 @@ def dump_unit_test():
         print("test_data unit test failed. Program exit.")
         exit()
 
-    train_df = pd.read_csv("/Users/lincong/Desktop/python_course/assignment/Dataset/ideal.csv")
+    ideal_df = pd.read_csv("/Users/lincong/Desktop/python_course/assignment/Dataset/ideal.csv")
     cursor.execute("SELECT * FROM ideal_functions LIMIT 1;")
     row = cursor.fetchone()
-    expected_row = ideal_df.iloc[0]  # 获取 CSV 第一行数据并转换成元组
+    expected_row = ideal_df.iloc[0]  
     sanity_check = True
     for i in range(len(row)):
         if row[i] != expected_row.iloc[i]:
